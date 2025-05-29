87b398ee2108ad9b32850db5496ba1c3c2bef02e
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-05-29 19:53:56 +0200

    atomic queue

---
+++ test/atomic/spsc_queue/include/atomic_queue.hpp
@@ -0,0 +1,35 @@
+#ifndef ATOMIC_QUEUE_HPP
+#define ATOMIC_QUEUE_HPP
+
+#include <atomic>
+#include <cstddef>
+#include <optional>
+
+template <typename T>
+class atomic_queue
+{
+public:
+    atomic_queue(size_t size)
+        : m_data(nullptr), m_size(size), m_head(0), m_tail(0)
+    {
+    }
+
+    inline size_t size() const { return m_size; }
+
+    void push(const T& value)
+    {
+        m_data[m_tail.load()] = value;
+        m_tail.store((m_tail + 1) % m_size);
+    }
+
+    std::optional<T> pop() {}
+
+    ~atomic_queue() {}
+
+private:
+    T* m_data;
+    size_t m_size;
+    std::atomic<size_t> m_head, m_tail;
+};
+
+#endif // ATOMIC_QUEUE_HPP
--- test/atomic/spsc_queue/src/main.cpp
+++ test/atomic/spsc_queue/src/main.cpp
@@ -1,11 +1,28 @@
 #include <cstdio>
 
-#include "spsc_queue.hpp"
+#include "atomic_queue.hpp"
+#include "thread_pool.hpp"
 
 int main(int argc, char** argv)
 {
-    std::printf("Hello World\n");
-    spsc_queue<int> queue(10);
+    thread_pool tp(8);
+
+    atomic_queue<int> queue(10);
+    auto produce = [&]() {
+        for (size_t i = 0; i < (1 << 15); i++)
+            queue.push(i);
+    };
+
+    auto consume = [&]() {
+        for (size_t i = 0; i < (1 << 15); i++)
+            queue.pop();
+    };
+
+    for (size_t i = 0; i < 20; i++)
+        tp.submit(produce);
+
+    for (size_t i = 0; i < 20; i++)
+        tp.submit(consume);
 
     return 0;
 }
