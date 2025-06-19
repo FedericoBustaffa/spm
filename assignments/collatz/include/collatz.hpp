ffd242a4d18d16f8b1db1101aa0b151424784f5f
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-06-19 16:43:53 +0200

    collatz

--- assignments/collatz/include/collatz.hpp
+++ assignments/collatz/include/collatz.hpp
@@ -17,8 +17,4 @@ double cyclic(size_t workers_num,
 double dynamic(size_t workers_num,
                const std::vector<std::pair<uint64_t, uint64_t>>& ranges);
 
-double dynamic_threadpool(
-    size_t workers_num,
-    const std::vector<std::pair<uint64_t, uint64_t>>& ranges);
-
 #endif
