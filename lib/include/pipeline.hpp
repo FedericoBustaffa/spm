3c219e5b954f73d5d3a392505fcbe712c06453c6
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-06-26 19:27:57 +0200

    pipeline and stage

--- lib/include/pipeline.hpp
+++ lib/include/pipeline.hpp
@@ -1,26 +1,37 @@
 #ifndef PIPELINE_HPP
 #define PIPELINE_HPP
 
-#include <functional>
 #include <thread>
 #include <vector>
 
 #include "mpmc_queue.hpp"
+#include "stage.hpp"
 
 namespace spm
 {
 
+template <typename In, typename Out>
 class pipeline
 {
 public:
     pipeline() {}
 
-    template <typename Func>
-    void add_stage(Func&& func)
+    template <typename... Args>
+    void add_stage(stage<Out, Args...>&& stage)
     {
-        auto work = []() {};
+        auto work = [&](mpmc_queue<In>&& in, mpmc_queue<Out>&& out) {
+            std::optional<In> input;
+            while (true)
+            {
+                input = in.pop();
+                if (!input.has_value())
+                    break;
 
-        m_stages.emplace_back(work, m_queues.front(), func);
+                stage(input);
+            }
+        };
+
+        m_stages.emplace_back(work, mpmc_queue<In>(), mpmc_queue<Out>(), stage);
     }
 
     template <typename T>
@@ -33,7 +44,6 @@ public:
 
 private:
     std::vector<std::thread> m_stages;
-    std::vector<mpmc_queue<...>> m_queues;
 };
 
 } // namespace spm
