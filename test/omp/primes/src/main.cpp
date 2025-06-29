726144ef8a710df2a4a223b9ca3325b6eaf8d2fd
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-06-29 18:31:41 +0200

    primes

--- test/omp/primes/src/main.cpp
+++ test/omp/primes/src/main.cpp
@@ -1,6 +1,6 @@
 #include <cstdint>
 #include <cstdio>
-#include <string>
+#include <omp.h>
 
 #include "timer.hpp"
 
@@ -31,12 +31,13 @@ int main(int argc, const char** argv)
 
     uint64_t primes = 0;
 
-#pragma omp parallel for reduction(+ : primes)
+#pragma omp parallel for reduction(+ : primes) schedule(dynamic)
     for (uint64_t i = 2; i <= n; i++)
         primes += is_prime(i);
 
     double time = timer.stop();
-    std::printf("time: %.2f\n", time);
+    std::printf("%d workers time: %.2f\n", omp_get_max_threads(), time);
+
     // std::printf("primes: %lu\n", primes);
 
     return 0;
