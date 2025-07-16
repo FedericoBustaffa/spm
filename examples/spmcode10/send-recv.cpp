f9a02877008fd719e2ba0a3e0d77cb3552a9aa48
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-07-16 19:23:09 +0200

    mpi

--- examples/spmcode10/send-recv.cpp
+++ examples/spmcode10/send-recv.cpp
@@ -1,26 +1,28 @@
-#include <string.h>
 #include <cstdio>
+#include <cstring>
 #include <mpi.h>
 
-int main (int argc, char *argv[]){
-	char message [20];
-	int myrank ;
-	MPI_Status status ;
-	MPI_Init (&argc, &argv);
-	MPI_Comm_rank (MPI_COMM_WORLD, &myrank);
-	if ( myrank == 0) /* code for process zero */
-	{
-		strcpy (message , "Hello, there");
-		MPI_Send(message, strlen(message)+1, MPI_CHAR, 1, 99, MPI_COMM_WORLD);
-	}
-	else if ( myrank == 1) /* code for process one */
-	{
-		MPI_Recv(message, 20, MPI_CHAR, 0, 99, MPI_COMM_WORLD, &status);
-		int count;
-		MPI_Get_count(&status, MPI_CHAR, &count);
-		printf("received: %s (MPI_CHAR received %d)\n ", message, count);
-	}
-	MPI_Finalize ();
-	return 0;
-}
+int main(int argc, char* argv[])
+{
+    char message[20];
+    int myrank;
+    MPI_Status status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    if (myrank == 0) /* code for process zero */
+    {
+        std::strcpy(message, "Hello, there");
+        size_t len = std::strlen(message) + 1;
+        MPI_Send(message, len, MPI_CHAR, 1, 99, MPI_COMM_WORLD);
+    }
+    else if (myrank == 1) /* code for process one */
+    {
+        MPI_Recv(message, 20, MPI_CHAR, 0, 99, MPI_COMM_WORLD, &status);
+        int count;
+        MPI_Get_count(&status, MPI_CHAR, &count);
+        std::printf("received: %s (MPI_CHAR received %d)\n", message, count);
+    }
+    MPI_Finalize();
 
+    return 0;
+}
--- test/mpi/hello_world/src/main.cpp
+++ test/mpi/hello_world/src/main.cpp
@@ -1,7 +1,24 @@
 #include <cstdio>
+#include <mpi.h>
 
-int main(int argc, const char** argv)
+int main(int argc, char** argv)
 {
-    std::printf("MPI\n");
+    MPI_Init(&argc, &argv);
+
+    int size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    std::printf("communicator size: %d\n", size);
+
+    char name[128];
+    int len;
+    MPI_Get_processor_name(name, &len);
+    std::printf("name: %s\n", name);
+
+    int id;
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+    std::printf("ID: %d\n", id);
+
+    MPI_Finalize();
+
     return 0;
 }
