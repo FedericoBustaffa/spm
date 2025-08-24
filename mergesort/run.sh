f104649c43fa6ff86643c380abdf57a5bc3c6957
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-08-24 12:05:43 +0200

    remove mergesort

--- mergesort/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-for i in "128MB" "256MB" "512MB" "1GB" "2GB" "3GB"; do
-    ./sequential.out $1 $i | grep -i "time"
-done
--- mergesort/slurm_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-for i in 2048 4096 8192 16384 32768 65536; do
-    srun ./sequential.out $1 $i
-done
